19cba9a1c271dfd00107195f69c56e43b8868e36
schoberl <tsibevili@gmail.com>
2016-04-18 17:23:36 +0200

    no message

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+    static MusicPlayer instance = null;
+
+    void Awake ()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self-destructing");
+        }
+        else
+        {
+            instance = this;
+        }
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
